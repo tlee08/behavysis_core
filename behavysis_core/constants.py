618ba8c3f36e49eae4d8df15cb6736a2f76e9c32
tlee08 <timothyeelim.lee@gmail.com>
2024-10-27 05:39:34 +0300

    abcd

--- behavysis_core/constants.py
+++ behavysis_core/constants.py
@@ -20,6 +20,9 @@ class Folders(Enum):
     FEATURES_EXTRACTED = "5_features_extracted"
     PREDICTED_BEHAVS = "6_predicted_behavs"
     SCORED_BEHAVS = "7_scored_behavs"
+    # ANALYSIS = "8_analysis"
+    ANALYSIS_COMBINED = "9_analysis_combined"
+    # EVALUATE = "10_evaluate"
 
 
 FILE_EXTS = {
@@ -31,12 +34,12 @@ FILE_EXTS = {
     Folders.FEATURES_EXTRACTED: ".feather",
     Folders.PREDICTED_BEHAVS: ".feather",
     Folders.SCORED_BEHAVS: ".feather",
+    Folders.ANALYSIS_COMBINED: ".feather",
 }
 
 # TODO: is there a better way to do the subsubdirs?
 DIAGNOSTICS_DIR = "0_diagnostics"
 ANALYSIS_DIR = "8_analysis"
-ANALYSIS_COMBINED_DIR = "9_analysis_combined"
 EVALUATE_DIR = "10_evaluate"
 TEMP_DIR = ".temp"
 
